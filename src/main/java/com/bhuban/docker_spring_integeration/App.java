7bb0d0ec6c315cb645f6d59ae0f057331e852f8f
Bhuban <bhubanbhaskar91@gmail.com>
2021-06-06 01:38:03 +0530

    first commit

---
+++ src/main/java/com/bhuban/docker_spring_integeration/App.java
@@ -0,0 +1,13 @@
+package com.bhuban.docker_spring_integeration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
